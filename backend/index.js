878956a41104dfc9db318b383543202d3a9e64c1
rehmeee <chrehmanali5@gmail.com>
2025-03-15 23:01:28 +0500

    Add Express server setup in backend/index.js

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+import express from "express";
+import http from "http";
+
+const app = express();
+const server = http.createServer(app);
+server.listen(3000, () => { console.log("Server is running on port 3000") });
