3a1176a30bba4c1e393176a4b9e5af4bca948dc8
rehmeee <chrehmanali5@gmail.com>
2025-03-19 22:11:06 +0500

    Add SignIn, SignUp, SessionDashboard, and TestHistoryDashboard components to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,10 @@ import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import SubjectsGrid from './components/SubjectsGrid.jsx'
 import Footer from './components/Footer.jsx'
+import SignIn from './components/SignIn.jsx'
+import SignUp from './components/SignUp.jsx'
+import SessionDashboard from './components/SessionDashboard.jsx'
+import TestHistoryDashboard from './components/TestHistoryDashboard.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,9 +16,13 @@ function App() {
   return (
     <>
     {/* <Navbar/> */}
-    <Hero/>
+    {/* <Hero/>
     <SubjectsGrid/>
-    <Footer/>
+    <Footer/> */}
+    {/* <SignIn/> */}
+    {/* <SignUp/> */}
+    {/* <SessionDashboard/> */}
+    <TestHistoryDashboard/>
     </>
   )
 }
