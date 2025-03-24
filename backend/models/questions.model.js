4ca8298fa4d64e563ae78fd4451b3092be4847c4
rehmeee <chrehmanali5@gmail.com>
2025-03-24 23:17:28 +0500

    Add Questions model with schema for subject, question text, choices, and correct answer

---
+++ backend/models/questions.model.js
@@ -0,0 +1,20 @@
+import mongoose, { mongo } from "mongoose";
+const questionSchema = new mongoose.Schema({
+    subject: {
+        type : String,
+        required: true
+    },
+    questionText : {
+        type: String,
+        required: true
+    }, 
+    choices: {
+        type: [String],
+        required: true
+    }, 
+    correctAnswer: {
+        type: [String],
+        required: true
+    }
+} , {timestamps: true})
+export const Questions =  mongoose.model("Questions", questionSchema);
