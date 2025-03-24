b0a5f0ffeb4e10d4b71971aafe36197553d9bd5b
rehmeee <chrehmanali5@gmail.com>
2025-03-24 23:17:56 +0500

    Add user routes to the application

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser";
+import userRouter  from "./routs/user.routes.js"
 
 const app = express();
 app.use(cors({
@@ -12,5 +13,5 @@ app.use(express.json({
     limit: "16kb"
 }))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-
+app.use("/user", userRouter);
 export {app};
