9e888bc460bc4cca81bcbb13aed7465319387510
rehmeee <chrehmanali5@gmail.com>
2025-04-06 22:32:38 +0500

    Add session slice for managing messages, questions, and users

---
+++ frontend/src/features/sessionSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { act } from "react";
+
+const initialState ={
+    messages:[{}],
+    question: [{}],
+    users:[{}],
+
+}
+const sessionSlice = createSlice({
+    name: session,
+    initialState,
+    reducers:{
+        addMessage: (state, action)=>{
+            const message = {
+                id : nanoid(),
+                messageText : action.payload.messageText,
+                userId: action.payload.userId
+            }
+            state.messages.push(message)
+        }, 
+        addQuestion: (state, action)=>{
+            state.question = action.payload
+        },
+        addUser : (state, action)=>{
+            state.users.push(action.payload)
+        }
+    }
+})
+export const {addMessage, addQuestion, addUser} = sessionSlice.actions
+
+export default sessionSlice.reducer
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit"
 
+import sessionSlice from "../src/features/sessionSlice.js"
+import authSlice from "../src/features/signInSlice.js"
 export const store = configureStore({
-    
+reducer:{
+    session: sessionSlice,
+    auth: authSlice
+}
+
 })
