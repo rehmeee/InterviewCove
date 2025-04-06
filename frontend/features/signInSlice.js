a79cadfe6ce2b01c1df61cee5d4f5a5d94ae83d3
rehmeee <chrehmanali5@gmail.com>
2025-04-06 22:14:12 +0500

    Add authentication slice for token management

---
+++ frontend/features/signInSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
+const refreshToken = localStorage.getItem("refreshToken")
+const initialState = {
+    token: refreshToken? refreshToken: null,
+
+}
+const authSlice = createSlice({
+    name: auth,
+    initialState,
+    reducers:{
+        addTokens : (state, action)=>{
+            state.token = action.payload
+        },
+        removeToken : (state, action)=>{
+            state.token = null
+        }
+    }
+})
+
+export const {addTokens, removeToken} = authSlice.actions
+
+export default authSlice.reducer
