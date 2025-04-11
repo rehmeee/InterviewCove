51e7a0ab4a440bec15243ed132e92a85571e06c2
rehmeee <chrehmanali5@gmail.com>
2025-04-11 21:02:25 +0500

    somethigs aded to leaderboard

--- frontend/src/features/leaderBoardSlice.js
+++ frontend/src/features/leaderBoardSlice.js
@@ -1,1 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios"
+let initialState ;
+  try {
+     initialState = await axios.get("https:localhost//5300/leaderboard").then(data=> console.log(data))
+} catch (error) {
+    
+}
+
+const leaderboardSlice = createSlice({
+    name: leaderboard,
+    initialState,
+    reducers:{
+        
+    }
+})
