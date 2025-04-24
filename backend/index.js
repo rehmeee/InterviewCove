4cba954b28e7c999f80ab26a90c2f35773bfae05
rehmeee <chrehmanali5@gmail.com>
2025-04-25 01:33:48 +0500

    Implement WebSocket server with user authentication and session management

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,57 @@
 import {app} from "./app.js"
 import dotenv from "dotenv"
 import dbConnect from "./db/dbConnection.js"
+import {Server} from "socket.io"
+import http from "http"
+import cors from "cors"
+import jwt from "jsonwebtoken"
+import { verifyUser } from "./middlewares/auth.middleware.js"
+import { User } from "./models/user.model.js"
+
 dotenv.config({
     path: "./.env"
 })
 dbConnect().then(()=>{
-    app.get("/" ,(req, res)=>{
-        res.send("hi you are on right port")
+    const server = http.createServer(app);
+    const io = Server(server, {
+        cors:{
+            origin: process.env.CORS_ORIGIN, 
+            Credential: true
+        }
     })
-    app.listen(5000, ()=>{
+    io.use(async(socket, next)=>{
+       try {
+         const token = socket.handshake.auth.token;
+         if(!token) {
+             return next(new Error("no token found"))
+         }
+         const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
+         const user = await User.findById(decoded._id).select("-password")
+         if(!user){
+             return next(new Error("user is not found"))
+         }
+         socket.user = user;
+         next();
+       } catch (error) {
+        return next(new Error("user is Unathurized"))
+       }
+    })
+    io.on("connection", socket=>{
+        socket.on("createSession" , async({roomId, questions, subject})=>{
+
+            socket.emit("sessionCreated", {})
+        });
+
+        socket.on("joinSession", async({roomId})=>{
+            socket.emit("sessionJoined", {});
+        })
+        
+
+    })
+    server.listen(5000, ()=>{
         console.log("app is listning on port 5000")
     })
-    app.on("error", (error)=>{
-        console.log(error)
-    })
+    
 }).catch((error)=>{
     console.log("error while connecting database ", error)
 })
